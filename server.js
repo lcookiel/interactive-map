cd60ede538cc38a8dad6e82b664a23855c30de31
Lazarus <eleazar.levchenko@gmail.com>
2021-07-04 19:49:19 +0300

    Remove deprecated body-parser module

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const connectDB = require('./server/database/connection');
